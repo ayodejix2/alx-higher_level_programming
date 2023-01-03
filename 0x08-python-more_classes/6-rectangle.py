4ade6c74d2e1c04a7d40acb34caac3fc9c4ff1ca
Ayodeji Ayodeji <110476398+ayodejix2@users.noreply.github.com>
2023-01-03 21:02:46 +0100

    task

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -6,14 +6,19 @@ Defines a class Rectangle
 
 class Rectangle:
     """Representation of a rectangle"""
+
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """Initializes the rectangle"""
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     def __del__(self):
         """prints a string when an instance has been deleted"""
         print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
 
     @property
     def width(self):
@@ -64,4 +69,3 @@ class Rectangle:
     def __repr__(self):
         """returns a string representation of the rectangle for reproduction"""
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
-      
